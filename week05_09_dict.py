98fd76b3b22c1f3cffdbdbdddd3027a4feb53cd7
kongmajitenshi <dksk3504@gmail.com>
2023-10-01 02:00:41 +0900

    231001_03

--- week05_09_dict.py
+++ week05_09_dict.py
@@ -3,3 +3,25 @@ infos = {
     "20230001":{"name":"kim inha", "age":21 },
     "20230002":{"name":"choi comsung", "age":22, "major":"컴정" }
 }
+
+# p.256
+for key, value in infos.items() : # key와 value 모두 뽑아줌
+    print(key, value)
+print()
+
+
+# p.226
+for value in infos.values() :
+    print(value)
+print()
+
+for key in infos.keys():
+    print(infos[key])
+print()
+
+for key in infos.keys():
+    print(key)
+print()
+
+for key in infos:
+    print(key)
